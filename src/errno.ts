1cc700db5486315b6f8be5ca0c9f910971043a73
German Meza <iam@germanamz.com>
2024-12-22 13:55:51 -0600

    make source unknown

--- src/errno.ts
+++ src/errno.ts
@@ -16,9 +16,9 @@ export class Errno<S extends number = number> extends Error implements ErrnoI<S>
 
   public status: S;
 
-  public source?: Error;
+  public source?: unknown;
 
-  constructor(code: string, message: string, status: S, context: unknown[] = [], source?: Error) {
+  constructor(code: string, message: string, status: S, context: unknown[] = [], source?: unknown) {
     super(message);
 
     this.code = code;
@@ -26,7 +26,7 @@ export class Errno<S extends number = number> extends Error implements ErrnoI<S>
     this.status = status;
     this.source = source;
 
-    if (source) {
+    if (source instanceof Error) {
       this.name = source.name;
       this.stack = source.stack;
     }
